47c38c8753797da984aa6981b2990d7446fce71d
Yilun Chen <yilunc@Yiluns-MBP-6.domain>
2017-05-16 23:50:30 -0700

    it kinda wokrs

--- portfolio.py
+++ portfolio.py
@@ -9,16 +9,18 @@ class Portfolio():
     self.last_value = 0
 
   def __str__(self):
+    str =  "---YOUR PORTFOLIO---\n"
     for ticker in self.stocks:
-      print("{0}: ").format(ticker)
+      str += ("\t{0}: \n").format(ticker)
       curr_share = Share(ticker)
       for num_shares, price in self.stocks[ticker]:
         basis = num_shares * price
-        curr_price = curr_share.get_price()
-        gain = curr_price * num_shares - basis
-        return_p = (curr_price-price)/price
-        print("{0} shares. Basis: ${1} | Curr. Price: ${2} | Gain: ${2} | Return: {2}%") \
+        curr_price = float(curr_share.get_price())
+        gain = float(curr_price * num_shares) - basis
+        return_p = float(curr_price-price)/float(price)
+        str += ("\t  {0} shares. Basis: ${1} | Curr. Price: ${2} | Gain: ${2} | Return: {2}% \n") \
           .format(num_shares, basis, curr_price, gain, return_p)
+    return str
 
   def add_shares(self, ticker, num_shares, price):
     if ticker in self.stocks:
--- portfolio_status.py
+++ portfolio_status.py
@@ -1,14 +1,24 @@
 from yahoo_finance import Share
-from Portfolio import Portfolio
+from portfolio import Portfolio
 import pickle
+import os
+import sys
 
-PICKLE_PATH = '~/Dev/investments/portfolio.pickle'
+PICKLE_PATH = 'portfolio.pickle'
 
 if not os.path.exists(PICKLE_PATH):
   with open(PICKLE_PATH, 'wb') as f:
-    pickle.dump(, f)
+    pickle.dump(Portfolio(), f)
 
 with open(PICKLE_PATH, 'rb') as f:
       portfolio = pickle.load(f)
 
-portfolio
+if len(sys.argv) == 2:
+  print(str(portfolio))
+elif len(sys.argv) == 5:
+  portfolio.add_shares(sys.argv[2], int(sys.argv[3]), float(sys.argv[4]))
+else:
+  portfolio.sell_shares(sys.argv[2], int(sys.argv[3]), float(sys.argv[4]), int(sys.argv[5]), float(sys.argv[6]))
+  
+with open(PICKLE_PATH, 'wb') as f:
+    pickle.dump(portfolio, f)
