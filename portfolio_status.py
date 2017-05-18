bae0e4e23a5349f0fb2df34e2e338be286f4bcae
Yilun Chen <yilunc@Yiluns-MBP-6.domain>
2017-05-17 23:38:45 -0700

    adding some inline documentation

--- portfolio_status.py
+++ portfolio_status.py
@@ -16,12 +16,14 @@ with open(PICKLE_PATH, 'rb') as f:
       portfolio = pickle.load(f)
 
 if len(sys.argv) == 2:
-  if sys.argv[1] == "s":
+  if sys.argv[1] == "r": # Prints a review of your portfolio to console.
     print(str(portfolio))
 elif len(sys.argv) == 5:
-  portfolio.add_shares(sys.argv[2], int(sys.argv[3]), float(sys.argv[4]))
+  if sys.argv[1] == "a": # Add a set of shares purchased at a price.
+    portfolio.add_shares(sys.argv[2], int(sys.argv[3]), float(sys.argv[4]))
 else:
-  portfolio.sell_shares(sys.argv[2], int(sys.argv[3]), float(sys.argv[4]), int(sys.argv[5]), float(sys.argv[6]))
+  if sys.argv[1] == "s": # Sell a set of shares purchased at a price for a selling price.
+    portfolio.sell_shares(sys.argv[2], int(sys.argv[3]), float(sys.argv[4]), int(sys.argv[5]), float(sys.argv[6]))
   
 with open(PICKLE_PATH, 'wb') as f:
     pickle.dump(portfolio, f)
